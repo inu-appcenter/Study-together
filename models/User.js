f10bfd27afdf2e41399dd3b3b7bf598c620753e5
Youngsik Yang <vacu9708@gmail.com>
2022-08-22 17:18:41 +0900

    feat: 네이버 로그인, 회원가입 추가

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    unique: true, // To prevent duplicate values
+    type: String
+  },
+  password: String,
+  name: String,
+  nickname: String,
+  email: String,
+  gender: String,
+  birthday: String,
+  img: String,
+})
+
+export default mongoose.model('user', userSchema);
